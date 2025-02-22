8260f1a19cb2099430fa120e7a5018a305dfc3f5
Gleb B <peremichka256@gmail.com>
2025-02-22 15:35:47 +0700

    Модуль 2. Создание и выбор примитивов

--- HelloNRX.cpp
+++ HelloNRX.cpp
@@ -8,40 +8,121 @@
 #ifdef _DEBUG
 #endif
 
-void helloNrxCmd()
+/// <summary>
+/// Функция добавления примитива в пространство модели
+/// </summary>
+void addToModelSpace(AcDbObjectId& objId, AcDbEntity* pEntity)
 {
-	//Запрос на ввод String
-	ACHAR str[133];
-	//Код результата работы функции
-	int ret;
-	ret = ncedGetString(1, L"\nВведите строку", str);
+	//Таблица
+	AcDbBlockTable* pBlockTable;
+	//Коллекция объектов из таблицы
+	AcDbBlockTableRecord* pBlock;
 
-	if (ret != RTCAN)
+	acdbHostApplicationServices()->workingDatabase()
+		->getSymbolTable(pBlockTable, AcDb::kForRead);
+	pBlockTable->getAt(ACDB_MODEL_SPACE, pBlock, AcDb::kForWrite);
+	pBlockTable->close();
+
+	pBlock->appendAcDbEntity(objId, pEntity);
+	pBlock->close();
+}
+
+/// <summary>
+/// Команда создания отрезка классичесим способом
+/// </summary>
+void helloNrxLine()
+{
+	AcDbLine* pLine = new AcDbLine(AcGePoint3d(0, 0, 0), AcGePoint3d(100, 100, 0));
+	addToModelSpace(pLine->objectId(), pLine);
+	pLine->close();
+}
+
+/// <summary>
+/// Команда создания отрезков при помощи транзакций
+/// </summary>
+void helloNxLineTran()
+{
+	AcTransaction* pTrans = actrTransactionManager->startTransaction();
+	AcDbDatabase* dB = acdbHostApplicationServices()->workingDatabase();
+
+	try 
 	{
-		ncutPrintf(L"\nВы ввели: %s", str);
+		//Открытие блока текущего пространства на запись
+		AcDbObject* pBlockTableObj = nullptr;
+		AcDbBlockTable* pBlockTable = nullptr;
+		pTrans->getObject(pBlockTableObj, dB->blockTableId(), AcDb::kForRead);
+		//Приводим указатель к типу BlockTable
+		pBlockTable = AcDbBlockTable::cast(pBlockTableObj);
+
+		AcDbBlockTableRecord* pModelSpace = nullptr;
+		pBlockTable->getAt(ACDB_MODEL_SPACE, pModelSpace, AcDb::kForWrite);
+
+		//Создание и добавление линии в блок
+		AcGePoint3d firstPoint;
+		int retFirst = ncedGetPoint(NULL, L"\nПоставьте первую точку ", asDblArray(firstPoint));
+		AcGePoint3d secondPoint;
+		int retSecond = ncedGetPoint(NULL, L"\nПоставьте вторую точку ", asDblArray(secondPoint));
+
+		AcDbLine* pLine = nullptr;
+
+		if (retFirst != RTCAN && retSecond != RTCAN)
+		{
+			pLine = new AcDbLine(AcGePoint3d(firstPoint.x, firstPoint.y, 0),
+				AcGePoint3d(secondPoint.x, secondPoint.y, 0));
+		}
+		else
+		{
+			pLine = new AcDbLine(AcGePoint3d(0, 0, 0), AcGePoint3d(-100, -100, 0));
+		}
+
+		pModelSpace->appendAcDbEntity(pLine->objectId(), pLine);
+		actrTransactionManager->addNewlyCreatedDBRObject(pLine);
+		actrTransactionManager->endTransaction();
+		pModelSpace->close();
+		pBlockTable->close();
+		pLine->close();
 	}
-
-	//Запрос на ввод Double
-	double realNumber = 0.0;
-	ret = ncedGetReal(L"\nВведите вещественное значение", &realNumber);
-
-	if (ret != RTCAN)
+	catch (const Acad::ErrorStatus)
 	{
-		ncutPrintf(L"\nВы ввели: %f", realNumber);
+		actrTransactionManager->abortTransaction();
 	}
+}
 
-	//Запрос на ввод Point (вводить через ,)
-	ncedInitGet(RSG_NONULL, L"Первое вТорое ещеОдно _ First sEcond oneMore");
-	nds_real point[3];
-	ret = ncedGetPoint(point, L"\nВведите координаты точки по x y z через ,: ", point);
+/// <summary>
+/// Команда с аффинными преобразованиями примитивов
+/// </summary>
+void helloNrxMove()
+{
+	ads_name ssname;
+	//Берём ВСЕ примитвы с чертежа
+	acedSSGet(L"A", NULL, NULL, NULL, ssname);
+	long length = 0;
+	acedSSLength(ssname, &length);
+	ads_name ent; 
 
-	if (ret == RTKWORD)
+	//В циле каждый примитив вертим
+	for (long i = 0; i < length; i++)
 	{
-		NCHAR kw[100];
-		ncedGetInput(kw);
-		//Выводит английское название, хотя может вводится русское
-		ncutPrintf(L"\nВами было введено ключевое слово: %s", kw);
+		acedSSName(ssname, i, ent); 
+		AcDbObjectId objId;
+		acdbGetObjectId(objId, ent); 
+		AcDbEntity* pEntity = nullptr;
+		acdbOpenAcDbEntity(pEntity, objId, AcDb::kForWrite);
+		//Сначала разворачиваем на 360
+		NcGeMatrix3d mat = NcGeMatrix3d::rotation(PI, NcGeVector3d::kZAxis);
+		pEntity->transformBy(mat);
+		//Увеличиваем в два раза
+		mat = NcGeMatrix3d::scaling(2, NcGePoint3d::kOrigin);
+		pEntity->transformBy(mat);
+		//Отражаем от оси Y
+		mat = NcGeMatrix3d::mirroring(NcGePlane::kYZPlane);
+		pEntity->transformBy(mat);
+		//Смещаем по оси Y на 1000 вверх
+		AcGeVector3d displacementVector(0, 1000, 0);	
+		mat = NcGeMatrix3d::translation(displacementVector);
+		pEntity->transformBy(mat);
 	}
+	acedSSFree(ssname);
 }
 
 // EntryPoint
@@ -55,7 +136,17 @@ ncrxEntryPoint(NcRx::AppMsgCode msg, void* pkt)
 			L"_HELLONRX",
 			L"HELLONRX",
 			ACRX_CMD_TRANSPARENT,
-			helloNrxCmd);
+			helloNrxLine);
+		ncedRegCmds->addCommand(L"HELLONRX_GROUP",
+			L"_HELLONRXTRAN",
+			L"HELLONRXTRAN",
+			ACRX_CMD_TRANSPARENT,
+			helloNxLineTran);
+		ncedRegCmds->addCommand(L"HELLONRX_GROUP",
+			L"_HELLONRXMOVE",
+			L"HELLONRXMOVE",
+			ACRX_CMD_TRANSPARENT,
+			helloNrxMove);
 		break;
 	case NcRx::kUnloadAppMsg:   
 		acedRegCmds->removeGroup(L"HELLONRX_GROUP");
